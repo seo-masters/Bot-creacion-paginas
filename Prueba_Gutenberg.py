e68cfffa9f2aec6564eaf4c6ba68dd8bb027b000
AndresBdull <andresbedullpl@gmail.com>
2023-09-30 09:30:53 -0500

    Creacion de titulos

--- Prueba_Gutenberg.py
+++ Prueba_Gutenberg.py
@@ -13,6 +13,7 @@ def insert_image(url):
     wait_and_click(driver, By.CLASS_NAME, 'components-button.block-editor-block-types-list__item.editor-block-list-item-uagb-image')
     wait_and_click(driver, By.CLASS_NAME, 'components-button.block-editor-media-placeholder__button.is-tertiary')
     wait_and_send_keys(driver, By.CLASS_NAME, 'block-editor-media-placeholder__url-input-field', url)
+    wait_and_click(driver, By.CLASS_NAME, 'components-button.block-editor-media-placeholder__url-input-submit-button.has-icon')
 
 def image_cover(driver):
     time.sleep(2)
@@ -40,16 +41,38 @@ def image_cover(driver):
     last_Btn.click()
 
 def insert_title(driver,title,size):
-    wait_and_insert_item(driver,"Title","components-button.block-editor-block-types-list__item.editor-block-list-item-post-title")
-    wait_and_send_keys(driver,By.CLASS_NAME,'block-editor-rich-text__editable.block-editor-block-list__block.wp-block.is-selected.wp-block-post-title.rich-text', title)
-    wait_and_send_keys(driver,By.CLASS_NAME,'block-editor-rich-text__editable.block-editor-block-list__block.wp-block.is-selected.wp-block-post-title.rich-text', Keys.ESCAPE)
+    wait_and_insert_item(driver,"Heading","components-button.block-editor-block-types-list__item.editor-block-list-item-heading")
+    time.sleep(1)
+    #cambia el texto del interior
+    wait_and_send_keys(driver,By.CLASS_NAME,'block-editor-rich-text__editable.block-editor-block-list__block.wp-block.is-selected.wp-block-heading.rich-text', title)
+    wait_and_send_keys(driver,By.CLASS_NAME,'block-editor-rich-text__editable.block-editor-block-list__block.wp-block.is-selected.wp-block-heading.rich-text', Keys.ESCAPE)
     wait_and_click(driver,By.CLASS_NAME,'components-button.block-selection-button_select-button')
     time.sleep(2)
+    #Selecciona el icono para cambiar los botones 
     wait_and_click(driver,By.XPATH,'//button[@aria-label="Change level"]')
-    wait_and_click(driver,By.XPATH,'//button[@aria-label="Change level"]')
+    time.sleep(2)
+    #Recupera todo el listado de opciones disponibles y Selecciona la ultima
     btns = driver.find_elements(By.XPATH,'//div[@aria-label="Change level"]//button')
-    btn = btns[size-1]
-    btn.click()
+    btn = btns[size]
+    aria_checked = btn.get_attribute('aria-checked')
+    
+    if aria_checked == 'true':
+        print("El boton no se puede seleccionar")
+    else:
+        btn.click()
+
+    #Selecciona el icono para alinear el texto
+    wait_and_click(driver,By.XPATH,'//button[@aria-label="Align text"]')
+    time.sleep(2)
+    #Recupera todo el listado de opciones disponibles y elige el numero 2
+    align = driver.find_elements(By.XPATH,'//div[@aria-label="Align text"]//button')
+    center = align[1]
+    center.click()
+    # Selecciona el ultimo icono de da opciones generales
+    wait_and_click(driver,By.CLASS_NAME,'components-dropdown.components-dropdown-menu.block-editor-block-settings-menu')
+    Btns = driver.find_elements(By.CSS_SELECTOR,'.components-menu-group button')
+    last_Btn = Btns[0]
+    last_Btn.click()
 
 def wait_and_click(driver, by, value):
     element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((by, value)))
@@ -89,21 +112,33 @@ wait_and_click(driver, By.ID, 'wp-submit')
 wait_and_click(driver, By.ID, 'menu-pages')
 wait_and_click(driver, By.CLASS_NAME, 'page-title-action')
 
+#creacion del contenedor
 container_columns(driver,0)
 
+#Insertar la imagen
 insert_image('https://magianegrachicago.com/wp-content/uploads/2023/07/amarres-en-chicago-efectivos-amarres-de-amor-chicago.jpg')
 
-wait_and_click(driver, By.CLASS_NAME, 'components-button.block-editor-media-placeholder__url-input-submit-button.has-icon')
-
+#Vuelve la imagen insertada en cover
 image_cover(driver)
 
-insert_title(driver,'Este es un titulo de ejemplo', 1)
+# Inserta un titulo de Tamaño H1
+insert_title(driver,'Este es un titulo de ejemplo', 0)
+insert_title(driver,'Este es un H2', 1)
+
+
+# Buttons
+# components-button block-editor-block-types-list__item editor-block-list-item-buttons
+# block-editor-rich-text__editable wp-block-button__link wp-element-button rich-text
+# block-editor-rich-text__editable wp-block-button__link wp-element-button rich-text
+# aria-label="Change items justification"
+# btns = driver.find_elements(By.XPATH,'//div[@aria-label="Change level"]//button')
+# btn = btns[1]
+# aria-label="Link"
+# block-editor-url-input__input
 
 time.sleep(60)
 driver.quit()
-# # Insertar título - sigue el mismo patrón
-# wait_and_click(driver, By.CLASS_NAME, 'components-button.block-editor-block-types-list__item.editor-block-list-item-post-title')
-# wait_and_send_keys(driver, By.CLASS_NAME, 'block-editor-rich-text__editable.block-editor-block-list__block.wp-block.is-selected.wp-block-post-title.rich-text', 'Hola Mundo')
+
 
 # # Insertar párrafo - sigue el mismo patrón
 # wait_and_click(driver, By.CLASS_NAME, 'components-button.block-editor-block-types-list__item.editor-block-list-item-paragraph')
@@ -113,6 +148,5 @@ driver.quit()
 # ...
 
 # Cerrar el navegador al finalizar
-
-
+ 
 # components-button block-editor-media-placeholder__button is-tertiary
